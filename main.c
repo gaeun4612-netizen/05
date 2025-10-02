e67a882a489c61d57d4e0fabc87167e6ad185422
gaeun4612-netizen <gaeun4612@gmail.com>
2025-10-02 15:21:11 +0900

    02

--- main.c
+++ main.c
@@ -6,16 +6,14 @@
 int main(int argc, char *argv[]) {
 	
 	int x;
-	 
-	printf("정수 하나를 입력하시오 :");
-    scanf("%d", &x);
-
-	if (x > 0)
-	  printf("양수입니다.\n");
-	else if (x < 0)
-	  printf("음수입니다.\n");
-	else 
-	  printf("0 입니다.\n");
+	
+	printf("정수 하나를 입력하세요 : ");
+	scanf("%d", &x);
+	
+	if (x < 0)
+	  printf("%d\n",-x);
+	else
+	  printf("%d",x);
   
 	return 0;
 }
